7553c99cf76e8b1cdee57e49a742593f2ff3d2c6
Pawel_Mitrega <pawel.mitrega1@wp.pl>
2021-12-17 17:18:28 +0100

    first commit

---
+++ src/storage.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Sebastian on 16.12.2021.
+//
+
+#include <algorithm>
+#include <stack>
+#include <optional>
+#include <vector>
+#include <iostream>
+#include "storage.hpp"
+#include <set>
+
+
+Package::Package() {
+
+}
