9576fbcdeeed293445da0dd6ce9170357645f9c9
Michal Michniak <michniak@student.agh.edu.pl>
2022-01-07 22:03:50 +0100

    nowe pliki

--- include/nodes.hpp
+++ include/nodes.hpp
@@ -6,28 +6,57 @@
 #define SIECI_NODES_HPP
 
 #include "types.hpp"
+#include "helpers.hpp"
+#include "storage_types.hpp"
+#include <map>
+#include <optional>
+
+/// definicja żeby nie było czerwono:
+class IPackageReceiver{
+};
 
 class ReceiverPreferences{
 public:
-
-
-private:
     using preferences_t = std::map<IPackageReceiver*, double>;
     using const_iterator = preferences_t::const_iterator;
+    ReceiverPreferences(ProbabilityGenerator pg): pg_(pg){};
+    void add_receiver( IPackageReceiver* r);
+    void remove_receiver(IPackageReceiver* r);
+    IPackageReceiver* choose_receiver();
+    preferences_t& get_preferences(){ return pref_;}
+    const_iterator cbegin(){ return pref_.cbegin(); }
+    const_iterator cend(){ return pref_.cend(); }
+    preferences_t::iterator begin(){ return pref_.begin(); }
+    preferences_t::iterator end(){ return pref_.end(); }
+private:
+    preferences_t pref_;
+    ProbabilityGenerator pg_;
 };
+
 class PackageSender{
 public:
-
+    PackageSender(PackageSender&& sender) : receiver_preferences_(sender.receiver_preferences_){};
+    void send_package();
+    std::optional<Package>& get_sending_buffer();
+    ~PackageSender() = default;
+protected:
+    void push_package(Package&&);
 private:
+    ReceiverPreferences receiver_preferences_;
 
 };
 
-class Ramp(public: PackageSender){
+class Ramp: public PackageSender {
 public:
-    Ramp(ElementID id)
+    Ramp(PackageSender &&sender, ElementID id, TimeOffset di) : PackageSender(std::move(sender)), id_(id), di_(di) {};
+    void deliver_goods(Time t);
+    TimeOffset get_delivery_interval(){ return di_; };
+    ElementID get_id(){ return id_; };
+    ~Ramp() = default;
 private:
-
-}
+    ElementID id_;
+    TimeOffset di_;
+};
 
 
 #endif //SIECI_NODES_HPP
--- src/nodes.cpp
+++ src/nodes.cpp
@@ -1,4 +1,32 @@
 //
 // Created by nikos on 07.01.2022.
 //
+#include "nodes.hpp"
 
+IPackageReceiver* ReceiverPreferences::choose_receiver(){
+
+    double centyl = pg_();
+    double dystrybution = 0;
+    for(auto i : pref_){
+        dystrybution += i.second;
+        if(centyl<dystrybution){
+            return i.first;
+        }
+    }
+    /// niepotrzebne !!!
+    return (*pref_.begin()).first;
+}
+void ReceiverPreferences::add_receiver( IPackageReceiver* r){
+    pref_.insert(std::pair<IPackageReceiver*, double>(r,1)); /// kaj prawdopodobieństwo? >:/
+    /// zapewnienie niezmiennika
+    double sum = 0;
+    for(auto i : pref_){
+        sum += i.second;
+    }
+    for(auto i : pref_){
+        i.second = i.second/sum;
+    }
+}
+void ReceiverPreferences::remove_receiver(IPackageReceiver* r){
+    pref_.erase(r);
+}
