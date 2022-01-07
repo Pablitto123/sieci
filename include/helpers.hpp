5df4ef7a4c81cfdd47a9306081bf07e0543e787d
Michal Michniak <michniak@student.agh.edu.pl>
2022-01-07 17:58:24 +0100

    nowe pliki

--- include/helpers.hpp
+++ include/helpers.hpp
@@ -9,6 +9,8 @@
 extern std::random_device rd;
 extern std::mt19937 rng;
 
+using ProbabilityGenerator = std::function<double()>;
+
 extern double default_probability_generator();
 
 extern ProbabilityGenerator probability_generator;
--- include/nodes.hpp
+++ include/nodes.hpp
@@ -5,4 +5,29 @@
 #ifndef SIECI_NODES_HPP
 #define SIECI_NODES_HPP
 
+#include "types.hpp"
+
+class ReceiverPreferences{
+public:
+
+
+private:
+    using preferences_t = std::map<IPackageReceiver*, double>;
+    using const_iterator = preferences_t::const_iterator;
+};
+class PackageSender{
+public:
+
+private:
+
+};
+
+class Ramp(public: PackageSender){
+public:
+    Ramp(ElementID id)
+private:
+
+}
+
+
 #endif //SIECI_NODES_HPP
